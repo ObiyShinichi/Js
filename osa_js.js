a2f3c6f8924bc591fa97d90e7582bf3428efbd8a
ObiyShinichi <34906905+ObiyShinichi@users.noreply.github.com>
2018-01-08 07:25:31 +0700

    Add files via upload

---
+++ osa_js.js
@@ -0,0 +1,24 @@
+</script> 
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function(){ var touch  = $('#resp-menu'); var menu  = $('.menu'); $(touch).on('click', function(e) { e.preventDefault(); menu.slideToggle(); }); $(window).resize(function(){ var w = $(window).width(); if(w > 767 && menu.is(':hidden')) { menu.removeAttr('style'); } });});
+//]]></script>
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function(){var str=location.href.toLowerCase();
+$('.top-menu ul li a').each(function(){if(str.indexOf(this.href.toLowerCase())>-1){$("li.highlight").removeClass("highlight");$(this).parent().addClass("highlight")}})}) 
+$(function(){
+var pull=$('#pull');menu=$('.top-menu ul');menuHeight=menu.height();$(pull).on('click',
+function(e){e.preventDefault();menu.slideToggle()});$(window).resize(function(){
+var w=$(window).width();
+if(w>320&&menu.is(':hidden')){menu.removeAttr('style')}})});
+//]]></script>
+<script async='async' type='text/javascript'>
+function downloadJSAtOnload() {var element = document.createElement(&#39;script&#39;);element.src = &quot;//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js&quot;;document.body.appendChild(element);}
+if (window.addEventListener) window.addEventListener(&quot;load&quot;, downloadJSAtOnload, false); else if (window.attachEvent) window.attachEvent(&quot;onload&quot;, downloadJSAtOnload); else window.onload = downloadJSAtOnload;
+</script>
+<script>
+//<![CDATA[
+function loadCSS(e,t,o){"use strict";var i=window.document.createElement("link"),s=t||window.document.getElementsByTagName("script")[0];i.rel="stylesheet",i.href=e,i.media="only x",s.parentNode.insertBefore(i,s),setTimeout(function(){i.media=o||"all"})}loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");;
+//]]>
+</script>
