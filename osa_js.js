87ecbfc28fade20cac556d409b94d5e1ec769995
ObiyShinichi <34906905+ObiyShinichi@users.noreply.github.com>
2018-01-08 07:26:36 +0700

    Update osa_js.js

--- osa_js.js
+++ osa_js.js
@@ -1,4 +1,3 @@
-</script> 
 <script type='text/javascript'>
 //<![CDATA[
 $(document).ready(function(){ var touch  = $('#resp-menu'); var menu  = $('.menu'); $(touch).on('click', function(e) { e.preventDefault(); menu.slideToggle(); }); $(window).resize(function(){ var w = $(window).width(); if(w > 767 && menu.is(':hidden')) { menu.removeAttr('style'); } });});
